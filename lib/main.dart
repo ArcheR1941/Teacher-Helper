221c5d87ce293b17ecd6594f09813e4123dd47cc
Unuku <sainaonoko@gmail.com>
2021-06-03 17:32:36 +0800

    almost finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,123 @@
 import 'package:flutter/material.dart';
-import 'package:random_string/random_string.dart';
+import 'dart:math';
 
-void main() => runApp(RandomApp());
-
-class RandomApp extends StatelessWidget {
+void main () => runApp(RandomSelector());
+class RandomSelector extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext cotntext) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DemoPage(),
+        debugShowCheckedModeBanner: false,
+        home: Home()
     );
   }
 }
+class Home extends StatefulWidget{
 
-class DemoPage extends StatefulWidget {
   @override
-  _DemoPageState createState() => _DemoPageState();
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
 }
+class _HomeState extends State<Home> {
+  List<String> names = [
 
-class _DemoPageState extends State<DemoPage> {
-  TextStyle text = TextStyle(fontSize: 30);
-  int number = 25;
-
+  ];
+  TextEditingController nameController = TextEditingController();
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text('Teacher helper'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey.shade200,
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+        appBar: AppBar(
 
-            ],
+          title:Text(
+            "Teachers helper",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w900
+            ),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
         ),
-      ),
+        backgroundColor: Colors.blueGrey,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: ListView.builder(
+                    itemCount: names.length,
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (direction) {
+                          setState(() {
+                            names.removeAt(index);
+                          });
+                        },
+                        child: ListTile(
+                          title: Text(names[index]),
+                        ),
+                      );
+                    }
+                )
+            ),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Text(
+                          "Add new name",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: TextField(
+                                controller: nameController,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(top: 10, bottom: 10),
+                                    isDense: true
+
+                                ),
+                              ),
+                            ),
+                            RaisedButton(
+                                child: Text("Add"),
+                                onPressed: () {
+                                  addToList();
+                                }
+                            ),
+                          ],
+                        ),
+                        Builder(
+                            builder: (context) => RaisedButton(
+                                child: Text("Get Random Name"),
+                                onPressed: () {
+                                  var random = Random();
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content :Text("Random name is:" +names [random.nextInt(names.length)]))
+                                  );
+                                }
+                            )
+                        )
+                      ],
+                    )
+                )
+            )
+          ],
+        )
     );
   }
+  void addToList() {
+    if (nameController.text.isNotEmpty) {
+      setState(() {
+        names.add(nameController.text);
+      });
+    }
+  }
 }
-
-
-
